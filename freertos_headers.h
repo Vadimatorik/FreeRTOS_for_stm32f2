2b3487ebe4e4229c1c278116863659eae86b7938
Vadimatorik <deryabkinda@mail.ru>
2017-06-18 13:53:39 +0700

    Добавил полностью сконфигурированный FreeRTOS для F2 с фиксированным режимом работы.

---
+++ freertos_headers.h
@@ -0,0 +1,22 @@
+#ifndef FREERTOS_FOR_STM32F2_FREERTOS_HEADERS_H_
+#define FREERTOS_FOR_STM32F2_FREERTOS_HEADERS_H_
+
+/*
+ * Данный файл предназначен для доступа к FreeRTOS методам
+ * из CPP кода.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
+#include "timers.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
